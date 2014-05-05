pytest-dev/pytest-cov

commit b980d6f53eb327f8e277efa071b3165230520388

Fixed multiprocessing_hook on Windows.

cov_core.py
@@ -8,23 +8,8 @@ import sys
 import os
 
 
-def multiprocessing_hook():
-    try:
-        import multiprocessing.util
-        multiprocessing.util.register_after_fork(multiprocessing_start,
-                                                 multiprocessing_start)
-    except ImportError:
-        pass
-
-
 def multiprocessing_start(obj):
-    cov = cov_core_init.init()
-    if cov:
-        import multiprocessing.util
-        multiprocessing.util.Finalize(None,
-                                      multiprocessing_finish,
-                                      args=(cov,),
-                                      exitpriority=1000)
+    cov_core_init.init()
 
 
 def multiprocessing_finish(cov):
@@ -32,12 +17,19 @@ def multiprocessing_finish(cov):
     cov.save()
 
 
+try:
+    import multiprocessing.util
+    multiprocessing.util.register_after_fork(multiprocessing_start,
+                                             multiprocessing_start)
+except ImportError:
+    pass
+
+
 class CovController(object):
     """Base class for different plugin implementations."""
 
     def __init__(self, cov_source, cov_report, cov_config, config=None, nodeid=None):
         """Get some common config used by multiple derived classes."""
-
         self.cov_source = cov_source
         self.cov_report = cov_report
         self.cov_config = cov_config
@@ -56,7 +48,6 @@ class CovController(object):
         os.environ['COV_CORE_SOURCE'] = UNIQUE_SEP.join(self.cov_source)
         os.environ['COV_CORE_DATA_FILE'] = self.cov_data_file
         os.environ['COV_CORE_CONFIG'] = self.cov_config
-        multiprocessing_hook()
 
     @staticmethod
     def unset_env():

cov_core_init.py
@@ -14,7 +14,10 @@ that code coverage is being collected we activate coverage based on
 info passed via env vars.
 """
 
+
 UNIQUE_SEP = '084031f3d2994d40a88c8b699b69e148'
+
+import cov_core
 
 
 def init():
@@ -55,7 +58,14 @@ def init():
             cov.erase()
             cov.start()
 
-            return cov
+            try:
+                import multiprocessing.util
+                multiprocessing.util.Finalize(None,
+                                              cov_core.multiprocessing_finish,
+                                              args=(cov,),
+                                              exitpriority=1000)
+            except ImportError:
+                pass
 
     except Exception:
         pass

commit bdaed91bc2f5e5584abdc503d91f78e7bcb0a1d9

Fixed API-breaking change.
Regression from b980d6f.

cov_core.py
@@ -13,7 +13,8 @@ def multiprocessing_start(obj):
 
 
 def multiprocessing_finish(cov):
-    cov.stop()
+    if cov._started:
+        cov.stop()
     cov.save()
 
 

cov_core_init.py
@@ -67,5 +67,7 @@ def init():
             except ImportError:
                 pass
 
+            return cov
+
     except Exception:
         pass