501cb110197375c052491265bc3629ce8c200120
David Szotten <davidszotten@gmail.com>
2016-05-10 18:04:36 +0100

    py3 compat

--- src/pytest_cov/plugin.py
+++ src/pytest_cov/plugin.py
@@ -1,6 +1,9 @@
 """Coverage plugin for pytest."""
 import os
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 import pytest
 import argparse
