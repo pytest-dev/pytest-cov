6bc08b4b92ffaebeace072985db9b508b858b00b
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-07-05 22:54:31 +0300

    Bump version: 2.2.1 → 2.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project = 'pytest-cov'
 year = '2016'
 author = 'pytest-cov contributors'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '2.2.1'
+version = release = '2.3.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
