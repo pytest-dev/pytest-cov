da661726e5f48bb75a49d0e89afe8e8f2d337dfc
hugovk <hugovk@users.noreply.github.com>
2017-11-21 09:20:40 +0200

    Drop support for EOL Python 3.3

--- ci/bootstrap.py
+++ ci/bootstrap.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     tox_environments = [line for line in tox_environments if line not in ['clean', 'report', 'docs', 'check']]
 
     template_vars = {'tox_environments': tox_environments}
-    for py_ver in '27 33 34 35 py'.split():
+    for py_ver in '27 34 35 py'.split():
         template_vars['py%s_environments' % py_ver] = [x for x in tox_environments if x.startswith('py' + py_ver)]
 
     for name in os.listdir(join("ci", "templates")):
