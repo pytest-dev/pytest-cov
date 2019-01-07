24e8b8ae250fdd197db950d95b1f1debd9b3853f
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-07 02:43:00 +0200

    Bump version: 2.6.0 → 2.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project = 'pytest-cov'
 year = '2016'
 author = 'pytest-cov contributors'
 copyright = '{}, {}'.format(year, author)
-version = release = '2.6.0'
+version = release = '2.6.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/pytest_cov/__init__.py
+++ src/pytest_cov/__init__.py
@@ -1,2 +1,2 @@
 """pytest-cov: avoid already-imported warning: PYTEST_DONT_REWRITE."""
-__version__ = "2.6.0"
+__version__ = "2.6.1"
