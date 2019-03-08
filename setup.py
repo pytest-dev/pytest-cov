e16324a6d8d7688bd8aa4140ad41061b692744b0
Daniel Hahler <git@thequod.de>
2019-03-08 14:26:10 +0100

    t30: use older pytest-xdist/pytest-forked

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(
             'fields',
             'hunter',
             'process-tests==2.0.2',
-            'pytest-xdist==1.25.0',
+            'pytest-xdist',
             'six',
             'virtualenv',
         ]
