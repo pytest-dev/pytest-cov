7cdd4c9dabe4e44b685f14a93efc083ff48f6725
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-05-15 23:30:00 +0300

    Avoid getting into infinite recursion when clearing off _pending_signal. Better to lose a _pending_signal.
    The alternative is to use a signal mask but that is not so portable (we can still add that for linux tho).
    
    Ref #294.

--- src/pytest_cov/embed.py
+++ src/pytest_cov/embed.py
@@ -97,8 +97,8 @@ def cleanup():
     _active_cov = None
     _cleanup_in_progress = False
     if _pending_signal:
-        _signal_cleanup_handler(*_pending_signal)
         _pending_signal = None
+        _signal_cleanup_handler(*_pending_signal)
 
 
 multiprocessing_finish = cleanup  # in case someone dared to use this internal
