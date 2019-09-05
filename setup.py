eb5952f28535fccd48d0ac96ed4c23f03a258d35
Hugo <hugovk@users.noreply.github.com>
2019-09-05 19:03:25 +0300

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -109,7 +109,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -125,7 +124,7 @@ setup(
         'pytest>=3.6',
         'coverage>=4.4'
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     extras_require={
         'testing': [
             'fields',
