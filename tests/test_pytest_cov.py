d3c382a7e7e933e9602f5a5445d03f6dc5138da0
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-01-12 15:58:03 +0200

    Skip this on 3.8+

--- tests/test_pytest_cov.py
+++ tests/test_pytest_cov.py
@@ -1100,7 +1100,7 @@ def test_run_target():
 
 @pytest.mark.skipif('sys.platform == "win32"', reason="multiprocessing support is broken on Windows")
 @pytest.mark.skipif('platform.python_implementation() == "PyPy"', reason="often deadlocks on PyPy")
-@pytest.mark.skipif('sys.version_info[:2] == (3, 8)', reason="deadlocks on Python 3.8, see: https://bugs.python.org/issue38227")
+@pytest.mark.skipif('sys.version_info[:2] >= (3, 8)', reason="deadlocks on Python 3.8, see: https://bugs.python.org/issue38227")
 def test_multiprocessing_pool_terminate(testdir):
     pytest.importorskip('multiprocessing.util')
 
