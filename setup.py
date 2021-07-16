965d2ddd96a927c8038a068ee27d0649bf654960
Thomas Grainger <tagrain@gmail.com>
2021-07-16 15:59:50 +0100

    Revert "Avoid using toml as extra (#472)"
    This reverts commit 34edfa1cf80f8d722dec4f587b10d2783f89a0f0.

--- setup.py
+++ setup.py
@@ -124,8 +124,7 @@ setup(
     ],
     install_requires=[
         'pytest>=4.6',
-        'coverage>=5.2.1',
-        'toml'
+        'coverage[toml]>=5.2.1'
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     extras_require={
