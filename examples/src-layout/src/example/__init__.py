3490ccb725758abfbd1162abd53e8a6c454d1f95
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-20 17:20:33 +0300

    Python 2.7 and 3.5 support was already dropped

--- examples/src-layout/src/example/__init__.py
+++ examples/src-layout/src/example/__init__.py
@@ -1,12 +1,2 @@
-import sys
-
-PY2 = sys.version_info[0] == 2
-
-
-if PY2:
-    def add(a, b):
-        return b + a
-
-else:
-    def add(a, b):
-        return a + b
+def add(a, b):
+    return a + b
