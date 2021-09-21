6395ecec756433194c05d34af490315b35dddafa
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-21 10:40:56 +0300

    Test conditional collection on PyPy and CPython

--- examples/adhoc-layout/example/__init__.py
+++ examples/adhoc-layout/example/__init__.py
@@ -1,2 +1,11 @@
-def add(a, b):
-    return a + b
+import platform
+
+# test merging multiple tox runs with a platform
+# based branch
+if platform.python_implementation() == "PyPy":
+    def add(a, b):
+        return a + b
+
+else:
+    def add(a, b):
+        return a + b
