c8d4963dd59f358d017c3857275bf5b070f7a3d7
Bruno Oliveira <nicoddemus@gmail.com>
2022-07-31 12:57:39 -0300

    Fix flake8 error

--- tests/test_pytest_cov.py
+++ tests/test_pytest_cov.py
@@ -1761,8 +1761,11 @@ def test_pth_failure(monkeypatch):
     monkeypatch.setattr(sys, 'stderr', buff)
     monkeypatch.setitem(os.environ, 'COV_CORE_SOURCE', 'foobar')
     exec(payload)
-    assert buff.getvalue() == '''pytest-cov: Failed to setup subprocess coverage. Environ: {'COV_CORE_SOURCE': 'foobar'} Exception: SpecificError()
-'''
+    expected = (
+        "pytest-cov: Failed to setup subprocess coverage. "
+        "Environ: {'COV_CORE_SOURCE': 'foobar'} Exception: SpecificError()\n"
+    )
+    assert buff.getvalue() == expected
 
 
 def test_double_cov(testdir):
