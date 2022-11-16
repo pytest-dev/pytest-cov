fceb9ea4aab6fd35117a2b9e9bc337f265c22a5b
Christian Clauss <cclauss@me.com>
2022-11-16 11:48:02 +0100

    Add Python 3.11 and PyPy 3.9 to the testing

--- setup.py
+++ setup.py
@@ -111,11 +111,11 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Testing',
@@ -133,7 +133,7 @@ setup(
         'pytest>=4.6',
         'coverage[toml]>=5.2.1'
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     extras_require={
         'testing': [
             'fields',
